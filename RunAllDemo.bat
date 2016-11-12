1c74f1a66d954f3e1794f4d4e6db70fcca8504f0
tishion <tishion@163.com>
2016-11-12 21:16:00 +0800

    Add errorcode, Support memory raw data.

---
+++ RunAllDemo.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+@COLOR 1A
+ECHO ========================Run all demo for x86==============================
+@CD .\output\demo\Win32\
+FOR /r .\ %%i IN (*.exe) DO (
+	ECHO %%i
+	%%i
+)
+
+ECHO ========================Run all demo for x64==============================
+@CD .\output\demo\x64\
+FOR /r .\ %%i in (*.exe) DO (
+	ECHO %%i
+	%%i
+)
+
+PAUSE
