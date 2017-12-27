7db264ad47e88f9937a7d0f24bfb4a850a04800c
Sheen (Shen) Tian <limshion@gmail.com>
2017-12-27 20:53:27 +0800

    Fix the bat file

--- RunAllDemo.bat
+++ RunAllDemo.bat
@@ -1,27 +1,37 @@
 @ECHO OFF
 @COLOR 1A
 ECHO ========================Run all demos for x86-Debug==============================
-@CD .\output\demo\x86\Debug
-FOR /r .\ %%i IN (*.exe) DO (
+@pushd
+@CD output\demo\x86\Debug
+FOR /r . %%i IN (*.exe) DO (
 	ECHO %%i
 	%%i
 )
+@popd
+
 ECHO ========================Run all demos for x86-Release==============================
-@CD .\output\demo\x86\Release
-FOR /r .\ %%i IN (*.exe) DO (
+@pushd
+@CD output\demo\x86\Release
+FOR /r . %%i IN (*.exe) DO (
 	ECHO %%i
 	%%i
 )
+@popd
 
 ECHO ========================Run all demos for x64-Debug==============================
-@CD .\output\demo\x64\Debug
-FOR /r .\ %%i in (*.exe) DO (
+@pushd
+@CD output\demo\x64\Debug
+FOR /r . %%i in (*.exe) DO (
 	ECHO %%i
 	%%i
 )
+@popd
+
 ECHO ========================Run all demos for x64-Release==============================
-@CD .\output\demo\x64\Release
-FOR /r .\ %%i in (*.exe) DO (
+@pushd
+@CD output\demo\x64\Release
+FOR /r . %%i in (*.exe) DO (
 	ECHO %%i
 	%%i
 )
+@popd
