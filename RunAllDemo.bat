2c562ae55a2f73713ee5f2c86cc8c872b97e4399
Sheen (Shen) Tian <limshion@gmail.com>
2017-12-27 21:07:54 +0800

    Fix the test script

--- RunAllDemo.bat
+++ RunAllDemo.bat
@@ -1,38 +1,37 @@
 @ECHO OFF
 @COLOR 1A
 ECHO ========================Run all demos for x86-Debug==============================
-pwd
-@pushd
-@CD output\demo\x86\Debug
-FOR /r . %%i IN (*.exe) DO (
-	ECHO %%i
+pushd .
+CD output\demo\x86\Debug
+FOR /r %%i IN (*.exe) DO (
+	ECHO Running:%%i
 	%%i
 )
-@popd
+popd
 
 ECHO ========================Run all demos for x86-Release==============================
-@pushd
-@CD output\demo\x86\Release
-FOR /r . %%i IN (*.exe) DO (
-	ECHO %%i
+pushd .
+CD output\demo\x86\Release
+FOR /r %%i IN (*.exe) DO (
+	ECHO Running:%%i
 	%%i
 )
-@popd
+popd
 
 ECHO ========================Run all demos for x64-Debug==============================
-@pushd
-@CD output\demo\x64\Debug
-FOR /r . %%i in (*.exe) DO (
-	ECHO %%i
+pushd .
+CD output\demo\x64\Debug
+FOR /r %%i in (*.exe) DO (
+	ECHO Running:%%i
 	%%i
 )
-@popd
+popd
 
 ECHO ========================Run all demos for x64-Release==============================
-@pushd
-@CD output\demo\x64\Release
-FOR /r . %%i in (*.exe) DO (
-	ECHO %%i
+pushd .
+CD output\demo\x64\Release
+FOR /r %%i in (*.exe) DO (
+	ECHO Running:%%i
 	%%i
 )
-@popd
+popd
