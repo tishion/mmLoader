81062f8eadee4c52514ff9714812184b2f95c96f
Sheen (Shen) Tian <limshion@gmail.com>
2017-12-27 20:16:01 +0800

    add nuget build

--- RunAllDemo.bat
+++ RunAllDemo.bat
@@ -1,14 +1,26 @@
 @ECHO OFF
 @COLOR 1A
-ECHO ========================Run all demo for x86==============================
-@CD .\output\demo\x86\
+ECHO ========================Run all demos for x86-Debug==============================
+@CD .\output\demo\x86\Debug
+FOR /r .\ %%i IN (*.exe) DO (
+	ECHO %%i
+	%%i
+)
+ECHO ========================Run all demos for x86-Release==============================
+@CD .\output\demo\x86\Release
 FOR /r .\ %%i IN (*.exe) DO (
 	ECHO %%i
 	%%i
 )
 
-ECHO ========================Run all demo for x64==============================
-@CD .\output\demo\x64\
+ECHO ========================Run all demos for x64-Debug==============================
+@CD .\output\demo\x64\Debug
+FOR /r .\ %%i in (*.exe) DO (
+	ECHO %%i
+	%%i
+)
+ECHO ========================Run all demos for x64-Release==============================
+@CD .\output\demo\x64\Release
 FOR /r .\ %%i in (*.exe) DO (
 	ECHO %%i
 	%%i
