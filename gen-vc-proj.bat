5142bf6de39d44939a1fa4e41cfd47e7cd975a40
Sheen Tian <5869938+tishion@users.noreply.github.com>
2021-02-19 15:11:17 +0800

    Update gen-vc-proj.bat

--- gen-vc-proj.bat
+++ gen-vc-proj.bat
@@ -3,6 +3,7 @@ cmake ^
     -B .build ^
     -G "Visual Studio 16 2019" ^
     -A Win32 ^
+    -DCMAKE_SYSTEM_VERSION=10.0.18362.0 ^
     -DCMAKE_INSTALL_PREFIX=./pacakge ^
     -DBUILD_SHELLCODE_GEN=TRUE ^
     -DBUILD_MMLOADER_DEMO=TRUE
