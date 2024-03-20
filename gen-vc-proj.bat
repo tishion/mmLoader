7e86e008243ec28ba3cfb9fcf3ae8dbaeb0a60e9
Sheen Tian <5869938+tishion@users.noreply.github.com>
2024-03-20 18:28:12 +0800

    Update gen-vc-proj.bat
    remove  visual studio version and windows sdk version

--- gen-vc-proj.bat
+++ gen-vc-proj.bat
@@ -1,9 +1,7 @@
 cmake ^
     -S . ^
     -B .build ^
-    -G "Visual Studio 17 2022" ^
     -A Win32 ^
-    -DCMAKE_SYSTEM_VERSION=10.0.18362.0 ^
     -DCMAKE_INSTALL_PREFIX=./pacakge ^
     -DBUILD_SHELLCODE_GEN=TRUE ^
     -DBUILD_MMLOADER_DEMO=TRUE
