bf16d88cfeff664ae21b1069e4a19fef2e43b07e
Sheen Tian <5869938+tishion@users.noreply.github.com>
2025-06-09 19:26:20 +0800

    feat: remove code optimization for shellcode generator

--- gen-win-x86_64.bat
+++ gen-win-x86_64.bat
@@ -1,7 +1,7 @@
 cmake ^
     -S . ^
-    -B .build ^
-    -A Win32 ^
+    -B .build/win64 ^
+    -A x64 ^
     -DCMAKE_INSTALL_PREFIX=./pacakge ^
     -DBUILD_SHELLCODE_GEN=TRUE ^
     -DBUILD_MMLOADER_DEMO=TRUE
